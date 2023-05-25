366c3da358d81cec03cadbe074da98bf7c12579b
kj-dev <agassi77.dev@gmail.com>
2023-05-25 18:05:32 +0900

    Convert to arrow function

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -1,6 +1,6 @@
 import './ExpenseDate.css'
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' })
   const day = props.date.toLocaleString('en-US', { day: '2-digit' })
   const year = props.date.getFullYear()
