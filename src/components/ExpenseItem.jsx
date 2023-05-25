b910cca3ebe2cecfcb9e847fbb4ca0356ff956b6
kj-dev <agassi77.dev@gmail.com>
2023-05-25 10:36:16 +0900

    Display data in jsx

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 5, 25)
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 239.22
+
   return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$231.22</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   )
