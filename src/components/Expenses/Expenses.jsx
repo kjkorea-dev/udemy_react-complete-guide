2f96b8e38ee01e4d649149ab09c5569c9adbf266
kj-dev <agassi77.dev@gmail.com>
2023-05-30 18:00:15 +0900

    Extract expenses list to new components

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
-import ExpenseItem from './ExpenseItem'
+import { useState } from 'react'
+
 import Card from '../UI/Card'
 import './Expenses.css'
 import ExpensesFilter from './ExpensesFilter'
-import { useState } from 'react'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2023')
@@ -16,19 +17,6 @@ const Expenses = (props) => {
     (expense) => expense.date.getFullYear().toString() === filteredYear
   )
 
-  let expensesContent = <p>No expenses found.</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -36,7 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   )
