148740ec77a3038be8b821323dc39dc4ec2661db
kj-dev <agassi77.dev@gmail.com>
2023-05-30 13:32:48 +0900

    Add expenses filter component

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -5,7 +5,6 @@ const ExpenseDate = (props) => {
   const month = props.date.toLocaleString('en-US', { month: 'long' })
   const day = props.date.toLocaleString('en-US', { day: '2-digit' })
   const year = props.date.getFullYear()
-  console.log(typeof year)
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
