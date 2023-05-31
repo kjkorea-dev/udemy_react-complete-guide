5f05e0b829039e09effef7d5bf6fb94c9d836bda
kj-dev <agassi77.dev@gmail.com>
2023-05-31 16:36:22 +0900

    Add expenses chart

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+import './Chart.css'
+import ChartBar from './ChartBar'
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value)
+  const totalMaximum = Math.max(...dataPointValues)
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          label={dataPoint.label}
+          maxValue={totalMaximum}
+        />
+      ))}
+    </div>
+  )
+}
+
+Chart.propTypes = {
+  dataPoints: PropTypes.array.isRequired,
+}
+
+export default Chart
---
+++ src/components/Chart/ChartBar.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import './ChartBar.css'
+
+const ChartBar = (props) => {
+  let barFillHeight = '0%'
+
+  if (props.maxValue > 0) {
+    barFillHeight = `${Math.round((props.value / props.maxValue) * 100)}%`
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  )
+}
+
+ChartBar.propTypes = {
+  value: PropTypes.number.isRequired,
+  maxValue: PropTypes.number.isRequired,
+  label: PropTypes.string.isRequired,
+}
+
+export default ChartBar
