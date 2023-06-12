358f7c81481c46beee443d3556286b28cfe98526
kj-dev <agassi77.dev@gmail.com>
2023-06-12 14:53:46 +0900

    Add dynamic inline css

--- src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -6,21 +6,36 @@ import './CourseInput.css'
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value !== '') {
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value)
   }
 
   const formSubmitHandler = (event) => {
     event.preventDefault()
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    }
     props.onAddGoal(enteredValue)
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : '#ccc' }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? 'red' : '#ccc',
+            background: !isValid ? 'salmon' : 'transparent',
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
