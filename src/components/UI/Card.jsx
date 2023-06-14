31e14485405d279c162a6ac10d4492b5ab26822a
kj-dev <agassi77.dev@gmail.com>
2023-06-14 15:16:37 +0900

    Add Card component

---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import classes from './Card.module.css'
+
+const Card = (props) => {
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  )
+}
+
+Card.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+}
+
+export default Card
