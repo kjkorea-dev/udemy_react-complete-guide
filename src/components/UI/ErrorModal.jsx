69d5c97e9a35303fd89b315fd3a58a7878a865e5
kj-dev <agassi77.dev@gmail.com>
2023-06-16 16:15:59 +0900

    Use react Portal

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,29 +1,60 @@
+import { Fragment } from 'react'
+import { createPortal } from 'react-dom'
 import PropTypes from 'prop-types'
 
 import Button from './Button'
 import Card from './Card'
 import classes from './ErrorModal.module.css'
 
+const Backdrop = (props) => (
+  <div className={classes.backdrop} onClick={props.onConfirm} />
+)
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  )
+}
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById('backdrop-root')
+      )}
+      {createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </Fragment>
   )
 }
 
 ErrorModal.propTypes = {
+  title: PropTypes.string,
+  message: PropTypes.string,
+  onConfirm: PropTypes.func,
+}
+
+Backdrop.propTypes = {
+  onConfirm: PropTypes.func,
+}
+
+ModalOverlay.propTypes = {
   title: PropTypes.string,
   message: PropTypes.string,
   onConfirm: PropTypes.func,
