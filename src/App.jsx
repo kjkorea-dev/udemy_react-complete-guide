5d46032fa27c484273fc0ee7fde7561a39ddf52f
kj-dev <agassi77.dev@gmail.com>
2023-06-22 15:41:21 +0900

    Use useEffect to prevent side effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
@@ -7,14 +8,24 @@ import MainHeader from './components/MainHeader/MainHeader'
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+
+    if (storedUserLoggedInInformation == '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     console.log(email, password)
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true)
   }
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false)
   }
 
