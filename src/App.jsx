07aa62c9e124f6f55561234d5f340087473f0606
kj-dev <agassi77.dev@gmail.com>
2023-07-04 14:15:32 +0900

    Initialize for section 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,5 @@
-import React, { useContext } from 'react'
-
-import Login from './components/Login/Login'
-import Home from './components/Home/Home'
-import MainHeader from './components/MainHeader/MainHeader'
-import AuthContext from './store/auth-context'
-
-function App() {
-  const authCtx = useContext(AuthContext)
-
-  return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!authCtx.isLoggedIn && <Login />}
-        {authCtx.isLoggedIn && <Home />}
-      </main>
-    </React.Fragment>
-  )
+const App = () => {
+  return <h1>Hello</h1>
 }
 
 export default App
