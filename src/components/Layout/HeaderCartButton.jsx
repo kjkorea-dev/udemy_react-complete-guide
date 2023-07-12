830ff02e9cbe8e65a53639ae9e2c542fb4d52f05
kj-dev <agassi77.dev@gmail.com>
2023-07-12 17:23:14 +0900

    Count items in cart button via context

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext)
+
+  const numberOfItems = cartCtx.items.reduce(
+    (acc, item) => acc + item.amount,
+    0
+  )
   return (
     <button className={classes.button} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   )
 }
