3e9c4195946c286af3144847560268935259deb7
kj-dev <agassi77.dev@gmail.com>
2023-07-13 15:56:14 +0900

    Add cart reducer

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,11 +1,45 @@
 import PropTypes from 'prop-types'
 import CartContext from './cart-context'
+import { useReducer } from 'react'
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+}
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD': {
+      const updatedItems = [...state.items, action.payload]
+      const updatedTotalAmount =
+        state.totalAmount + action.payload.price * action.payload.amount
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      }
+    }
+    case 'REMOVE': {
+      break
+    }
+    default:
+      return defaultCartState
+  }
+}
 
 const CartProvider = (props) => {
+  const [cartState, dispatchCartState] = useReducer(
+    cartReducer,
+    defaultCartState
+  )
+
   // eslint-disable-next-line no-unused-vars
-  const addItemToCartHandler = (item) => {}
+  const addItemToCartHandler = (item) => {
+    dispatchCartState({ type: 'ADD', payload: item })
+  }
   // eslint-disable-next-line no-unused-vars
-  const removeItemFromCartHandler = (id) => {}
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartState({ type: 'REMOVE', payload: id })
+  }
 
   const cartContext = {
     items: [],
