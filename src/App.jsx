7ee106fc11ef38a4a6371adf0bdae719a636f157
kj-dev <agassi77.dev@gmail.com>
2023-10-23 17:07:58 +0900

    Set loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import MoviesList from './components/MoviesList'
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   async function fetchMoviesHandler() {
+    setIsLoading(true)
     const response = await fetch('https://swapi.dev/api/films')
     const data = await response.json()
 
@@ -18,6 +20,7 @@ const App = () => {
       }
     })
     setMovies(transformedData)
+    setIsLoading(false)
   }
 
   return (
@@ -26,7 +29,9 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </>
   )
