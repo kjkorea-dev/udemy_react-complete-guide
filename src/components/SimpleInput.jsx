3f5c0700a5f1d70f5e944c7af558ffa95f9c6692
kj-dev <agassi77.dev@gmail.com>
2023-12-13 17:20:13 +0900

    Handle user input using useRef and useState

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,9 +1,35 @@
+import { useRef, useState } from 'react'
+
 const SimpleInput = () => {
+  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState('')
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value)
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+    console.log(enteredName)
+
+    const enteredVvalue = nameInputRef.current.value
+    console.log(enteredVvalue)
+
+    // nameInputRef.current.value = ''  Not ideal, don't manipulate DOM
+    setEnteredName('')
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          ref={nameInputRef}
+          onChange={nameInputHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
