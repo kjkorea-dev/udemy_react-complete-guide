a8f6a45638e0b65fafbf1c143864e929e38752c5
kj-dev <agassi77.dev@gmail.com>
2023-12-14 17:40:05 +0900

    Add full form validation

--- src/components/SimpleInput.jsx
+++ src/components/SimpleInput.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState('')
   const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+  const [formIsValid, setFormIsValid] = useState(false)
 
   const enteredNameIsValid = enteredName.trim() !== ''
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      setFormIsValid(true)
+    }
+  }, [enteredNameIsValid])
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value)
@@ -49,7 +56,7 @@ const SimpleInput = () => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   )
